5982f649fb3494ef94a6f643d78521b4a1e5ec2e
Granit <gzymeri55@gmail.com>
2022-02-03 12:25:06 +0100

    commit tree bfa6cafbc7e3f0ebb12310188c42b39013e265e6

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+
+
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob'
+}
+
+const leastFavoriteCustomer = 'bob :('
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'NOT BOB'
+}
